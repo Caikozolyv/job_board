1acc46f4e54857772917c1c99c352c1d4a26c505
caikozolyv <valentinfarcy@gmail.com>
2025-08-14 12:49:26 +0200

    adding webpack encore, sass, vue and connecting them with project

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+import './styles/app.scss';
+import { createApp } from 'vue';
+import App from './vue/App.vue';
+
+createApp(App).mount('#app');
---
+++ assets/vue/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      hello: 'hello world!'
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>{{ hello }}</h1>
+</template>
