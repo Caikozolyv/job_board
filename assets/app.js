463cf351efc01c7b4d5246c069a7373f3d15b67e
caikozolyv <valentinfarcy@gmail.com>
2025-08-21 00:15:00 +0200

    adding bootstrap-vue-next package for styling

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,14 @@
 import './styles/app.scss';
 import { createApp } from 'vue';
+import { createBootstrap } from 'bootstrap-vue-next/plugins/createBootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
-// Fonction pour monter des composants dynamiquement
+// Function to mount components dynamically
 window.mountVueComponent = function(componentName, selector, props = {}) {
-    console.log(componentName, selector, props);
     import(`./vue/components/${componentName}.vue`).then(module => {
-        createApp(module.default, props).mount(selector);
+        createApp(module.default, props)
+            .use(createBootstrap())
+            .mount(selector);
     });
 };
